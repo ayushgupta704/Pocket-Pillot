19577b7146350bfa2b13a2c4d0e02d3ab035e273
Kush <reach2ayushgupta@gmail.com>
2025-02-16 01:03:20 +0530

    in dashboard create account added

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -7,7 +7,7 @@ const DashboardLayout = ()=>{
     <div className='px-5'>
         <h1 className='text-6xl font-bold gradient-title mb-5'>Dashboard</h1>
 
-        <DashboardPage/>
+        {/* <DashboardPage/> */}
         {/* suspense show loading indicator */}
         <Suspense fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea"/>}>
 
